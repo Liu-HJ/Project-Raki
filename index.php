82bd8df61f2991c8ba4a924437c330de3da6230c
WaterMonster <50351182+Liu-HJ@users.noreply.github.com>
2020-08-04 15:23:00 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<html lang="Zh-CN">
+<head>
+  <meta charset="utf-8">
+  <style type="text/css"> 
+<!-- 
+A { text-decoration: none} 
+--> 
+</style>
+
+<!--流量统计代码-->
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8d4faa9515320a7006eb30343ad1165e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+	<link rel="shortcut icon" href="icon.png" />
+	<link rel="bookmark" href="icon.png" />
+  <title>Prject Raki</title>
+  <!-- Link CSS -->
+  <link rel="stylesheet" href="/css/index.css">
+  <link rel="stylesheet" href="/css/button_index.css">
+</head>
+<body>
+    <div id="head" class="head-div">
+		<div id="head-title">
+		<a href=index.php><h1>Project Raki</h1></a>
+		</div>
+	</div>
+	
+	<div class="container">
+		<div class="card">
+			<a href="A.php"><h2>A</h2></a>
+		</div>
+		<div class="card">
+			<a href="BC.php"><h2>BC</h2></a>
+		</div>
+		<div class="card">
+			<a href="D.php"><h2>D</h2></a>
+		</div>
+		<div class="card">
+			<a href="E.php"><h2>E</h2></a>
+		</div>
+	    <div class="card">
+			<a href="https://afdian.net/@WaterMonster" target="_blank"><h2>捐</h2></a>
+		</div>
+	</div>
+	
+
+		<div id="page-bottom">
+		<div class="btn">
+		<div class="btn-back">
+			<p><H2>建议使用电脑访问<br>投稿请联系水怪<br>喜欢的话可以捐助我喔<br></H2></p>
+			<button class="yes"><H2>OK</H3></button>
+			<!--<button class="no">No</button> -->
+		</div>
+		<div class="btn-front"><h2>使 用 说 明</h2></div>
+	</div>
+
+<script src="/js/somee.js"> </script>
+			<br><br><br><br><span style="width:100%;text-align:center;display:inline-block;"><a href="http://www.beian.miit.gov.cn">蜀ICP备20009952号</a></span>
+		</div>
+</body>
+</html>
