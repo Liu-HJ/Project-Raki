1832968f2aca8c052848abb627856b1f2295c2c6
WaterMonster <50351182+Liu-HJ@users.noreply.github.com>
2020-08-04 15:30:46 +0800

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <title>404</title>
+    <meta charset="utf-8">
+	<link rel="shortcut icon" href="icon.png" />
+	<link rel="bookmark" href="icon.png" />
+	<script language="javascript" src="jquery.js"></script>
+	<link rel="stylesheet" href="/css/index.css" type="text/css" />
+	<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8d4faa9515320a7006eb30343ad1165e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+</head>
+<body>
+	    <div id="head" class="head-div">
+	        <div id="head-title">
+            <a href=/index.php><h1>Project Raki<br>404-页面不存在</h1><br></a>
+		    </div>
+		</div>
+		<div id="page-bottom">
+			<br><br><br><br><span style="width:100%;text-align:center;display:inline-block;"><a href="http://www.beian.miit.gov.cn">蜀ICP备20009952号</a></span>
+		</div>
+</body>
+</html>
---
+++ A.php
@@ -0,0 +1,78 @@
+<html>
+<head>
+    <title>Project Raki</title>
+    <meta charset="utf-8">
+	<link rel="shortcut icon" href="icon.png" />
+	<link rel="bookmark" href="icon.png" />
+	<script language="javascript" src="jquery.js"></script>
+<!--	<link rel="stylesheet" href="common.css" type="text/css" /> -->
+    <link rel="stylesheet" href="/css/style.css">
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?8d4faa9515320a7006eb30343ad1165e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
+</head>
+
+<body>
+    
+	<div id="head" class="head-div">
+		<div id="head-title">
+            <a href=/index.php><h1>Project Raki</h1></a>
+		</div>
+	</div>
+
+			
+<br>
+<div id="main">
+<div class="container">
+		<div class="card">
+			<div class="content">
+				<h2>高等数学</h2>
+				<p>在大一学习，包括四门理论课，由同顺老师授课。</p>
+				<a href="/A/Math.php"><b> 相 关 资 料 </b></a>
+			</div>
+		</div>
+		<div class="card">
+			<div class="content">
+				<h2>公共计算机</h2>
+				<p>包括计算机基础（理）与数据结构与算法。主要讲授计算基础、C++、算法与数据结构。</p>
+				<a href="#"><b> 相 关 资 料 </b></a>
+			</div>
+		</div>
+		<div class="card">
+			<div class="content">
+				<h2>大学语文</h2>
+				<p>该课程可以在任选学期进行学习，有多位老师供选择。</p>
+				<a href="#"><b> 相 关 资 料 </b></a>
+			</div>
+		</div>
+		<div class="card">
+			<div class="content">
+				<h2>大学物理</h2>
+				<p>在大一下学期与大二上学期学习，包括四门理论课与一门实验课。</p>
+				<a href="/A/Physics.php"><b> 相 关 资 料 </b></a>
+			</div>
+		</div>
+		<div class="card">
+			<div class="content">
+				<h2>体育</h2>
+				<p>至少修4门，之后所修算E类课学分。</p>
+				<a href="#"><b> 相 关 资 料 </b></a>
+			</div>
+		</div>
+	</div>
+	
+</div>
+
+		<div id="page-bottom">
+		   
+			<br><br><br><br><span style="width:100%;text-align:center;display:inline-block;"><a href="http://www.beian.miit.gov.cn">蜀ICP备20009952号</a></span>
+		</div>
+</body>
+</html>
