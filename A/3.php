2f283286124b709da93e2a87e78b6d3ae8d8277d
WaterMonster <50351182+Liu-HJ@users.noreply.github.com>
2020-08-11 13:25:45 +0800

    三级页面

---
+++ A/3.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="Zh-CN">
+<head>
+  <meta charset="utf-8">
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "";#统计链接
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+	<link rel="shortcut icon" href="/icon.png" />
+	<link rel="bookmark" href="/icon.png" />
+  <title>Project Raki</title>
+  <!-- Link CSS -->
+  <link rel="stylesheet" href="/css/style2.css">
+  <link rel="stylesheet" href="/css/button.css">
+</head>
+<body>
+    <div id="head" class="head-div">
+		<a href=/index.php><h1>Project Raki</h1></a>
+	</div>
+	<div id="head_back" class="head-div">
+		<a href=/A.php><h1> <- </h1></a>
+	</div>
+	<div id="container" class="container">
+		<div class="box">
+			<h2>01</h2>
+			<h3>Test</h3>
+			<p>Test<br>		
+			―――Test
+			</p>
+		</div>
+		<div class="box">
+			<h2>02</h2>
+			<h3>Test</h3>
+			<p>Test<br>		
+			―――Test
+			</p>
+		</div>
+		<div class="box">
+			<h2>03</h2>
+			<h3>Test</h3>
+			<p>Test<br>		
+			―――Test
+			</p>
+		</div>
+		
+	</div>
+	
+		<div id="page-bottom">
+		    
+		<div class="btn">
+		<div class="btn-back">
+			<p><H2>LINE1<br>
+      LINE2<br>
+      LINE3</H2></p>
+			<button class="yes"><H2>OK</H2></button>
+			<!--<button class="no">No</button> -->
+		</div>
+		<div class="btn-front"><h2>BUTTON</h2></div>
+	</div>
+	<script src="/js/somee.js"> </script>
+			<br><br><br><br><span style="width:100%;text-align:center;display:inline-block;"><a href="http://www.beian.miit.gov.cn">这里输入备案号</a></span>
+		</div>
+</body>
+</html>
