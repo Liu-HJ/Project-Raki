7056936388f7db4056239c31cb58984c7f6bdcc9
WaterMonster <50351182+Liu-HJ@users.noreply.github.com>
2020-08-11 13:28:04 +0800

    二级页面

--- A.php
+++ A.php
@@ -11,7 +11,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?8d4faa9515320a7006eb30343ad1165e";
+  hm.src = "";#统计链接，可以删除
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
@@ -30,49 +30,30 @@ var _hmt = _hmt || [];
 			
 <br>
 <div id="main">
-<div class="container">
+	<div class="container">
 		<div class="card">
 			<div class="content">
-				<h2>高等数学</h2>
-				<p>在大一学习，包括四门理论课，由同顺老师授课。</p>
-				<a href="/A/Math.php"><b> 相 关 资 料 </b></a>
+				<h2>三级页面</h2>
+				<p>这里是简介。</p>
+				<a href="/A/3.php"><b> 点此前往</b></a>
 			</div>
 		</div>
+		
 		<div class="card">
 			<div class="content">
-				<h2>公共计算机</h2>
-				<p>包括计算机基础（理）与数据结构与算法。主要讲授计算基础、C++、算法与数据结构。</p>
-				<a href="#"><b> 相 关 资 料 </b></a>
+				<h2>三级页面</h2>
+				<p>这里是简介。</p>
+				<a href="/A/3.php"><b> 点此前往</b></a>
 			</div>
 		</div>
-		<div class="card">
-			<div class="content">
-				<h2>大学语文</h2>
-				<p>该课程可以在任选学期进行学习，有多位老师供选择。</p>
-				<a href="#"><b> 相 关 资 料 </b></a>
-			</div>
-		</div>
-		<div class="card">
-			<div class="content">
-				<h2>大学物理</h2>
-				<p>在大一下学期与大二上学期学习，包括四门理论课与一门实验课。</p>
-				<a href="/A/Physics.php"><b> 相 关 资 料 </b></a>
-			</div>
-		</div>
-		<div class="card">
-			<div class="content">
-				<h2>体育</h2>
-				<p>至少修4门，之后所修算E类课学分。</p>
-				<a href="#"><b> 相 关 资 料 </b></a>
-			</div>
-		</div>
+	
 	</div>
 	
 </div>
 
 		<div id="page-bottom">
 		   
-			<br><br><br><br><span style="width:100%;text-align:center;display:inline-block;"><a href="http://www.beian.miit.gov.cn">蜀ICP备20009952号</a></span>
+			<br><br><br><br><span style="width:100%;text-align:center;display:inline-block;"><a href="http://www.beian.miit.gov.cn">在这里输入备案号</a></span>
 		</div>
 </body>
 </html>
